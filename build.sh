8e44d5416b9086e6240702046c2c3e3d639bc645
Alex Shaw <alex.shaw.as@gmail.com>
2021-05-05 16:03:11 -0400

    Add template system.

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+declare -a Projects=(
+    # "csv2table"
+    "SqlTest"
+    "litexa-ts"
+    "standard-release"
+    "9-11"
+)
+
+PROJECTS=""
+PROJECTS_FILE=$(mktemp)
+YAML_FILE=$(mktemp)
+REGEX="# @PROJECTS@"
+
+for name in ${Projects[@]}
+do
+    str=$(sed 's/^/  /g' projects/${name}.yml)
+    PROJECTS="${PROJECTS}${str}\n"
+done
+
+echo -e "$PROJECTS" > "$PROJECTS_FILE"
+
+sed -e "/$REGEX/r $PROJECTS_FILE" alex.yml > "$YAML_FILE"
+
+if [ -e "./pandoc" ]
+then
+    PANDOC="${PWD}/pandoc"
+else
+    PANDOC=$(which pandoc)
+fi
+
+${PANDOC} --template resume.pandoc.tex --pdf-engine=lualatex -o resume.pdf -f markdown "$YAML_FILE"
