85d5c2b3524b22b9dbdc97b676ba442b5ffa09ff
Alex <alex.shaw.as@gmail.com>
2024-11-20 20:37:31 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 declare -a Projects=(
-    # "csv2table"
+    #"csv2table"
     "SqlTest"
     "computestream"
     #"spotifyxr"
-    #"standard-release"
-    "9-11"
+    "standard-release"
+    #"9-11"
     #"simpleble"
     "litexa-ts"
 )
