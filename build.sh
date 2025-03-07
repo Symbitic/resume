00317a70920b497731abe7504762138b0653f3dc
Alex <alex.shaw.as@gmail.com>
2025-03-06 19:03:25 -0800

    Update for Adobe

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 declare -a Projects=(
-    "csv2table"
+    #"csv2table"
     "SqlTest"
     #"spotifyxr"
     "computestream"
     #"standard-release"
-    #"9-11"
+    "9-11"
     #"simpleble"
     "litexa-ts"
 )
